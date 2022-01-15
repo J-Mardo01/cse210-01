1d5d324fc94b9fcb0a00cabdbf28e25c92d6d7c4
J-Mardo01 <mar20080@byui.edu>
2022-01-15 13:47:12 -0700

    Update tictactoe.py
    Just a quick edit to make the output look cleaner.

--- tictactoe.py
+++ tictactoe.py
@@ -60,11 +60,11 @@ def player_input():
     while True:
         if player1.upper() == 'X':
             player2='O'
-            print("You've chosen" + player1 + ". Player 2 will be " + player2)
+            print("You've chosen " + player1 + ". Player 2 will be " + player2)
             return player1.upper(),player2
         elif player1.upper() == 'O':
             player2='X'
-            print("You've chosen" + player1 + ". Player 2 will be " + player2)
+            print("You've chosen " + player1 + ". Player 2 will be " + player2)
             return player1.upper(),player2
         else:
             player1 = input("Please pick a marker: 'X' or 'O' ")
